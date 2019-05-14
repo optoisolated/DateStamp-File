3ccb0a8fe6442b36e2f1d3a9366849cc0448e1f3
Optoisolated <46586989+optoisolated@users.noreply.github.com>
2019-05-15 09:18:34 +1000

    Create Function-DateStamp-File.ps1

---
+++ Function-DateStamp-File.ps1
@@ -0,0 +1,28 @@
+# DateStamp-File Function
+# By Optoisolated
+#
+# This function renames a file to include it's datestamp in the format
+# of C:\Filepath\OriginalFileName-YYYYMMDD.ext
+#
+# Example for Created Date
+#   DateStamp-File -FileName "C:\Sourcefile.png" -WhichDate "Created"
+# Example for Modified Date
+#   DateStamp-File -FileName "C:\Sourcefile.png" -WhichDate "Modified" 
+#
+
+Function DateStamp-File
+ {
+    param([string]$FileName, [string]$WhichDate)
+    $FileData = Get-Item $FileName 
+    Switch ($WhichDate)
+    {
+        Created  { $FileDate = $($FileData.CreationTime)  }
+        Modified { $FileDate = $($FileData.LastWriteTime) }
+    }
+    $OutDate = $FileDate.ToString('yyyyMMdd')
+    $NewFile = $($FileData.BaseName + "-" + $OutDate + $FileData.Extension)
+    "Renaming $($FileData.Name) to $NewFile "
+    Rename-Item -Path $FileName -NewName $NewFile
+ }
+ 
+ 
